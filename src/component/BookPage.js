c10f6308f0363c7ce9df629f5850d228b72be930
Hidayet Argandabi <hidayet.argandabi@turknet.net.tr>
2020-02-24 15:17:44 +0300

    inial

---
+++ src/component/BookPage.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+export default class BookPage extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            book : {
+                title: ''
+            }
+        }
+    }
+
+    handleChange = (event) => {
+        const book = {...this.state.book, title: event.target.value}
+        this.setState({
+            book: book
+        })
+        // log console
+        console.log('New State: ', this.state.book)
+    }
+
+
+    render() {
+        return (
+                <form>
+                    <h2>Books</h2>
+                    <input 
+                        type="text" 
+                        onChange={this.handleChange}
+                        value={this.state.book.title}
+                    />
+                    <input 
+                        type='submit'
+                        value='save'
+                    />
+                    
+                </form>
+        )
+    }
+}
